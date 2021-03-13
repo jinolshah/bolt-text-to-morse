f6cb720683d708d6eff4e9fc95eac3396704a3c5
Jinol <jinolshah08@gmail.com>
2021-03-13 14:22:35 +0530

    Added auto webpage open

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import DataInput
 from textmorse import converter, hitter
 from boltiot import Bolt
+import webbrowser
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hyf56gcxz9m11qa0'
-
-
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
@@ -22,5 +22,7 @@ def base():
         hitter(codeout, opchoice, mybolt)
     return render_template("home.html", form = form)
 
+webbrowser.open_new("http://localhost:5000/")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
